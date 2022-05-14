b19703b46a7af72dac9caa45ec101821a4a3e0fa
leesh <lsh80165@gmail.com>
2022-05-14 21:16:19 +0900

    baekjoon_2022_05_14

---
+++ 2022_05_14/문서_검색.py
@@ -0,0 +1,32 @@
+#백준 1543번 (문자열 , 실버4)
+import sys
+
+input_string = sys.stdin.readline().strip()
+
+search_string = sys.stdin.readline().strip()
+
+result = 0
+
+index = 0
+
+while index <= (len(input_string) - len(search_string)):
+
+    
+
+
+    if input_string[index] == search_string[0]:
+        print("tes")
+        for i in range(len(search_string)):
+            if input_string[index+i] != search_string[i]:
+                index += 1
+                break
+        else:
+            index += (i+1)
+            result += 1
+    else:
+        index+= 1
+
+    print(index)
+    
+
+print(result)
